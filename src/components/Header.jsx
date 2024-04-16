4c77635434f11856370bfcb050a7d14a17bb0641
abdullo Nishanov <abdullohnishanov2007@gmail.com>
2024-04-16 19:38:27 +0600

    created new folder pages and removed file Home.jsx, then created new file Header.jsx and removed to components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <nav>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
