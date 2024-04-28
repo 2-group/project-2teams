5d00162e1d6866983e6273e35b88a03d22634487
aizirek jusupbaeva <aizirekjusupbaeva@gmail.com>
2024-04-28 11:33:07 +0600

    i created header, then others pages home, turs, contacts, about and connected pages with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from './components/Header';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <BrowserRouter>
+     <Header/>
+      <Routes>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import logo from '../components/assets/logo.jpg'
+import { Link } from 'react-router-dom'
+import style from '../components/styles/styles.moduls.css'
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand as={Link} to='home'href="#">
+          <img style={{maxHeight: '150px', maxWidth: '150px'}} src={logo} alt="" />
+          
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -18,30 +24,33 @@ function Header() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
+            <Nav.Link as={Link} to='about' href="Что вас ждет">Что вас ждет</Nav.Link>
+            <Nav.Link as={Link} to='contacts' href="contacts">Контакты</Nav.Link>
+            <NavDropdown style={{color:'grey'}} title="Поездка" id="navbarScrollingDropdown">
+              <NavDropdown.Item as={Link} to='https://beket.kg/?ysclid=lvj2qkk66w314817953' href="tickets">Билеты</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='https://101hotels.com/kyrgyzstan/bishkek/best?ysclid=lvj2kqgz9i953328585' href="hotels">
+                Отели
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
+              <NavDropdown.Item to='https://experience.tripster.ru/destinations/kyrgyzstan/' as={Link} href="excursions">
+                Экскурсии
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+            <Nav.Link as={Link} to='turs'  href="#" >
+              Горящие туры
             </Nav.Link>
           </Nav>
+
+
+
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
-              className="me-2"
+              placeholder="Куда хотите отправиться?"
+              className="me-1"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success">Поиск</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
